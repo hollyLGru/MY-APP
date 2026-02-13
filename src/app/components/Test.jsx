e3a4c469cf1da3e4b891631e67611d0f8c34fae4
Holly Grudovich <hlgrudovich@gmail.com>
2026-02-13 13:42:38 -0700

    listings

--- src/app/components/Test.jsx
+++ src/app/components/Test.jsx
@@ -1,9 +1,10 @@
 'use client'
 import { useState, useEffect, useRef, useMemo } from 'react'
+import { listings } from '../data/listings'
 
 export default function Test() {
   const [term, setTerm] = useState('World')
-
+  console.log(listings)
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       hello {term}
