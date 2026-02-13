fed0bdf8de63e367934d00b42bbdd4e0f1636b83
Holly Grudovich <hlgrudovich@gmail.com>
2026-02-13 14:02:38 -0700

    images working, tailwind set up yeyyyy

--- src/app/components/Test.jsx
+++ src/app/components/Test.jsx
@@ -4,10 +4,13 @@ import { listings } from '../data/listings'
 
 export default function Test() {
   const [term, setTerm] = useState('World')
-  console.log(listings)
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       hello {term}
+      <div className="text-5xl font-bold text-pink-500">
+        Tailwind is alive 💅
+      </div>
+      <img className="w-auto" src={listings[0].images[0]} alt="Workflow logo" />
     </div>
   )
 }
