5a47a970833354a0bacace06ec88d8f5ad8845fa
Holly Grudovich <hlgrudovich@gmail.com>
2026-03-09 12:59:05 -0600

    Update page.jsx

--- src/app/map/page.jsx
+++ src/app/map/page.jsx
@@ -1,20 +1,12 @@
 import ListingsMap from '@/components/Map/ListingsMap'
 import listings from '@/data/listings'
 import Header from '@/components/Header'
-// import generateSummitCountyListings from '@/data/generateSummitCountyListings'
 
 export default async function Home() {
-  // const generatedSummitCountyListings = generateSummitCountyListings(100, {
-  //   county: 'summit-ut',
-  //   includeImages: true,
-  // })
   return (
     <div>
       <Header />
-      <ListingsMap
-        listings={listings}
-        // generatedSummitCountyListings={generatedSummitCountyListings}
-      />
+      <ListingsMap listings={listings} />
     </div>
   )
 }
