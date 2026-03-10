cfd87ad1e3f34fd54a73aa044cb40b689d707f52
Holly Grudovich <hlgrudovich@gmail.com>
2026-03-10 08:23:25 -0600

    delete test file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
-import Test from '@/components/Test'
-
 export default async function Home() {
-  console.log('here?')
-  const elevationRes = await fetch(
-    `  https://api.open-elevation.com/api/v1/lookup?locations=41.161758,-8.583933`
-  )
-  const elevation = await elevationRes.json()
-
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Test />
+      {/* <Test /> */}
+      find my ride
     </div>
   )
 }
